ace9b598729d4dc8d3e7634f4ac59706ad5ff613
Adam Isakov <99123480+adambala@users.noreply.github.com>
2024-01-26 02:15:08 +0300

    codewars: solved "Convert a Boolean to a String" kata.

---
+++ codewars/Python/Convert a Boolean to a String/main.py
@@ -0,0 +1,8 @@
+"""
+8 kyu: Convert a Boolean to a String
+Kata: https://www.codewars.com/kata/551b4501ac0447318f0009cd/
+"""
+
+
+def boolean_to_string(boolean):
+    return "True" if boolean else "False"
