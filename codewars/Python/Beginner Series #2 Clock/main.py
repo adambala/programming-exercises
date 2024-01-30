c24d230c35737b3327b93af42c1d738b49fa0445
Adam Isakov <99123480+adambala@users.noreply.github.com>
2024-01-31 01:38:33 +0300

    codewars: Python - solved "Beginner Series #2 Clock".

---
+++ codewars/Python/Beginner Series #2 Clock/main.py
@@ -0,0 +1,8 @@
+"""
+8 kyu: Beginner Series #2 Clock
+Kata: https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/
+"""
+
+
+def past(h: int, m: int, s: int) -> int:
+    return (h*3600 + m*60 + s)*1000
